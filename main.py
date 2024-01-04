73ab46b44a301e5779fccf7355ff33b0f79411c2
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-01-04 23:57:59 +0300

    hiii

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-print('hello, world!')
-print('1')
-pass
+import pygame
+
+
+
+pygame.init()
+width, height = 600, 800
+screen = pygame.display.set_mode((600, 800))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(60)
