6551cf217c132f1a57e9ad29a75055b2298ba981
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-01-07 01:16:36 +0300

    промежуточный вариант игры сидение ч.1.3 end of part 1

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = cell_size_y  # размер больших квадратов
-        self.small_size_rect_x = 30  # ширина узких квадратов
+        self.small_size_rect_x = 50  # ширина узких квадратов
         self.small_size_rect_y = cell_size_y  # длин. узких квадратов
-        self.stroka = 2 #проверка на какой строчке находится рендж в создании сетки
+        self.row_spacing = 20  # Расстояние между строками
         self.xs = ['black', 'red']
 
     def set_view(self, left, top, cell_size):
@@ -28,38 +28,61 @@ class Board:
         self.cell_size = cell_size
 
     def render(self, screen):
-        screen.fill((0, 0, 0))
-        for i in self.board:
-            self.stroka += 1
+        screen.fill(BLACK)
+        top_y = self.top
+        for row_index, row in enumerate(self.board):
             top_x = self.left
-            top_y = self.top + self.cell_size * self.stroka
-            for j in i:
-                if j == 1:
-                    pygame.draw.rect(screen, 'white', (top_x, top_y, self.small_size_rect_x, self.small_size_rect_y), 1)
+            row_height = 0  # Высота текущей строки
+            for col_index, cell in enumerate(row):
+                if cell == 1:
+                    pygame.draw.rect(screen, WHITE, (top_x, top_y, self.small_size_rect_x, self.small_size_rect_y), 1)
                     top_x += self.small_size_rect_x
+                    row_height = max(row_height, self.small_size_rect_y)
                 else:
-                    pygame.draw.rect(screen, 'white', (top_x, top_y, self.cell_size, self.cell_size), 1)
+                    color = RED if cell == 5 else WHITE
+                    pygame.draw.rect(screen, color, (top_x, top_y, self.cell_size, self.cell_size),
+                                     1 if cell == 0 else 0)
                     top_x += self.cell_size
+                    row_height = max(row_height, self.cell_size)
+            top_y += row_height + self.row_spacing
 
-    # def get_cell(self, mouse_pos):
-    #     x, y = mouse_pos
-    #     if self.left <= x <= self.left + self.cell_size * self.width and self.top <= y <= self.top + self.cell_size * self.height:
-    #         col = (x - self.left) // self.cell_size
-    #         row = (y - self.top) // self.cell_size
-    #         return (row, col)
-    #
-    #     else:
-    #         return None
-    #
-    # def on_click(self, cell_coords):
-    #     if cell_coords is not None:
-    #         row, col = cell_coords
-    #         if self.board[row][col] == 0:
-    #             self.board[row][col] = 1
-    #
-    # def get_click(self, mouse_pos):
-    #     cell = self.get_cell(mouse_pos)
-    #     self.on_click(cell)
+    def get_cell(self, mouse_pos):
+        x, y = mouse_pos
+        top_y = self.top
+
+        for row_index, row in enumerate(self.board):
+            top_x = self.left
+            row_height = 0
+
+            # Определяем высоту строки, чтобы использовать ее позже для проверки попадания
+            for cell in row:
+                if cell == 1:
+                    row_height = max(row_height, self.small_size_rect_y)
+                else:
+                    row_height = max(row_height, self.cell_size)
+
+            # Проверяем попадание в ячейки текущей строки
+            for col_index, cell in enumerate(row):
+                cell_width = self.small_size_rect_x if cell == 1 else self.cell_size
+                if top_x <= x < top_x + cell_width and top_y <= y < top_y + row_height:
+                    return (row_index, col_index)
+                top_x += cell_width
+
+            # Переходим к следующей строке, учитывая промежуток между строками
+            top_y += row_height + self.row_spacing
+
+        return None
+
+    def on_click(self, cell_coords):
+        if cell_coords is not None:
+            row, col = cell_coords
+            if self.board[row][col] == 0:
+                self.board[row][col] = 5
+                print(5)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
 
 
 pygame.init()
@@ -73,8 +96,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        #     board.get_click(event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+
+            board.get_click(event.pos)
     board.render(screen)
     pygame.display.flip()
 
