b351a68362c7391266796630f34ac61138af0747
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-01-08 21:51:31 +0300

    обьединение 2.2 END?

--- Alice.py
+++ Alice.py
@@ -1,11 +1,13 @@
 import pygame
 import numpy as np
 from pandas.core.common import flatten
+import copy
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+PINK = (255, 100, 100)
 LEVEL = {
     'Level 1': [
         [0, 1, 0, 1, 0, 1, 0, 1, 0],
@@ -16,11 +18,14 @@ LEVEL = {
 }
 
 
+
+
+
 class Board:
     def __init__(self, cell_size_y, name):
         self.width = 5
         self.height = 4
-        self.board = LEVEL[name]
+        self.board =  copy.deepcopy(LEVEL[name])
         self.left = 10
         self.top = 10
         self.cell_size = cell_size_y  # размер больших квадратов
@@ -85,6 +90,7 @@ class Board:
             row, col = cell_coords
             if self.board[row][col] == 0:
                 self.board[row][col] = 5
+                print(5)
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -99,20 +105,34 @@ class Board:
 
 
 class Button:
-    def __init__(self, x, y, width, height, color, text=''):
+    def __init__(self, x, y, width, height, act_color, inact_color, text=''):
         self.x, self.y = x, y
         self.width = width
         self.height = height
-        self.color = color
+        self.act_color = act_color
+        self.inact_color = inact_color
         self.text = text
 
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', 20)
-            text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+    def draw(self, win, x, y):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if y < mouse[1] < y + self.height and x < mouse[0] < x + self.width and click[0] == 1:
+            pygame.draw.rect(screen, (self.act_color), (x, y, self.width, self.height))
+            restart()
+            return 1
+        else:
+            pygame.draw.rect(screen, (self.inact_color), (x, y, self.width, self.height))
+
+def restart():
+    global board, start_ticks
+    screen.fill(BLACK)
+    board = Board(100, 'Level 1')
+    board.render(screen)
+    start_ticks = pygame.time.get_ticks()
+    pygame.display.flip()
+
+
+
 
 
 if __name__ == '__main__':
@@ -129,21 +149,19 @@ if __name__ == '__main__':
     start_ticks = pygame.time.get_ticks()  # стартовое время в миллисекундах
 
     # Создаем кнопки один раз вне цикла
-    button_done = Button(300, 600, 260, 50, RED, 'done')
-    button_retry = Button(500, 510, 260, 50, WHITE, 'не опять, а снова')
+    button_done = Button(300, 600, 260, 50, RED, PINK, 'done')
+    ##button_retry = Button(500, 510, 260, 50, WHITE, 'не опять, а снова')
 
     # Определите шрифт один раз вне цикла
     font = pygame.font.Font('freesansbold.ttf', 64)
 
     while running:
-
         millis = pygame.time.get_ticks() - start_ticks
         secs = millis // 1000
         if secs >= 16:
             secs = 15
         if board.proov():
             secs = time_of_end
-
         mins = secs // 60
         secs %= 60
 
@@ -158,23 +176,32 @@ if __name__ == '__main__':
                 x, y = event.pos
                 if secs < 15:
                     board.get_click(event.pos)
+
+                    # board.proov()
                 board.render(screen)
 
         # Рендеринг экрана и объектов
         screen.fill(BLACK)
         board.render(screen)
-        button_done.draw(screen)
         screen.blit(text, textRect)
 
-        if millis // 1000 >= 15 or board.proov():
+        if board.proov():
             time_of_end = secs
+            Tab = pygame.font.SysFont('arial', 100)
+            sc_Text = Tab.render('Yup', True, BLUE, WHITE)
+            cOr = sc_Text.get_rect(center=(1000, 200))
+            screen.blit(sc_Text, cOr)
+        if millis // 1000 >= 15:
             tab = pygame.font.SysFont('arial', 30)
             sc_text = tab.render('Вы нe успели! Попробуете выполнить задание заново?', True, WHITE, BLUE)
             cor = sc_text.get_rect(center=(1000, 200))
             screen.blit(sc_text, cor)
-            button_retry.draw(screen)
+            if button_done.draw(screen, 500, 600) == 1:
+                pygame.display.update()
+            ##button_retry.draw(screen)
 
         pygame.display.flip()
         clock.tick(60)  # Ограничить до 60 кадров в секунду
 
     pygame.quit()
+
