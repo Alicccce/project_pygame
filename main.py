fefdd2e5b6e8d30e71f2e014320cdab8af308464
aliis <aliissa.mrak@mail.ru>
2024-01-10 23:07:02 +0300

    задание 2 А

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ def load_image(name, colorkey=None):
     return image
 
 
-
 if __name__ == '__main__':
     pygame.init()
 
@@ -48,7 +47,7 @@ if __name__ == '__main__':
     size = (1300, 750)
     image = load_image("itog.png")
     screen = pygame.display.set_mode(size)
-    drawing_surface = pygame.Surface(size, pygame.SRCALPHA)
+    drawing_surface = pygame.Surface(image.get_size(), pygame.SRCALPHA)
 
     clock = pygame.time.Clock()
     start_ticks = pygame.time.get_ticks()  # стартовое время в миллисекундах
@@ -57,16 +56,20 @@ if __name__ == '__main__':
     # Определите шрифт один раз вне цикла
     font = pygame.font.Font('freesansbold.ttf', 64)
 
+
+    checkpoints = [(320, 87), (221, 262), (68, 432), (441, 423), (68, 267)]
+    checkpoints_x = [320, 221, 68, 441, 68]
+    checkpoints_y = [87, 262, 432, 423, 267]
+    s = []
+
     while running:
+        check = []
         millis = pygame.time.get_ticks() - start_ticks
         secs = millis // 1000
         if secs >= 16:
             secs = 15
         mins = secs // 60
         secs %= 60
-
-
-
 
         text = font.render('{}:{}'.format(mins, secs), True, WHITE, BLACK)
         textRect = text.get_rect(center=(1000, 400))
@@ -76,23 +79,41 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 x, y = event.pos
-            if event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONDOWN :
-                if pygame.mouse.get_pressed()[0]:
-                    pygame.draw.circle(drawing_surface, BLUE, event.pos, 5)
-                    pygame.draw.circle(screen, BLUE, event.pos, 5)
-
+            if event.type == pygame.MOUSEMOTION or event.type == pygame.MOUSEBUTTONDOWN:
+                if millis // 1000 >= 15:
+                    if len(s) != 5:
+                        tab = pygame.font.SysFont('arial', 26)
+                        sc_text = tab.render('Не получилось :( Попробуете выполнить задание заново?', True, WHITE, BLUE)
+                        cor = sc_text.get_rect(center=(900, 250))
+                        screen.blit(sc_text, cor)
+                    else:
+                        tab = pygame.font.SysFont('arial', 26)
+                        sc_text = tab.render('Всё GOOD', True, WHITE, BLUE)
+                        cor = sc_text.get_rect(center=(900, 250))
+                        screen.blit(sc_text, cor)
+                else:
+                    if pygame.mouse.get_pressed()[0]:
+                        pygame.draw.circle(drawing_surface, BLUE, event.pos, 7.5)
+                        dopustm = 0
+                        for i in range(30):
+                            if (event.pos[0] + dopustm in checkpoints_x and event.pos[1] + dopustm in checkpoints_y):
+                                check.append((event.pos[0] + dopustm, event.pos[1] + dopustm))
+                            if (event.pos[0] - dopustm in checkpoints_x and event.pos[1] - dopustm in checkpoints_y):
+                                check.append((event.pos[0] - dopustm, event.pos[1] - dopustm))
+                            if (event.pos[0] + dopustm in checkpoints_x and event.pos[1] - dopustm in checkpoints_y):
+                                check.append((event.pos[0] - dopustm, event.pos[1] - dopustm))
+                            if (event.pos[0] - dopustm in checkpoints_x and event.pos[1] + dopustm in checkpoints_y):
+                                check.append((event.pos[0] - dopustm, event.pos[1] - dopustm))
+                            dopustm += 1
+        for ch in check:
+            for hc in checkpoints:
+                if ch == hc and ch not in s:
+                    s.append(ch)
 
         screen.blit(text, textRect)
         button_done.draw(screen, 850, 600)
-
-        if millis // 1000 >= 15:
-            tab = pygame.font.SysFont('arial', 32)
-            sc_text = tab.render('Вы нe успели! Попробуете выполнить задание заново?', True, WHITE, BLUE)
-            cor = sc_text.get_rect(center=(900, 250))
-            screen.blit(sc_text, cor)
-
-        screen.blit(image, (10, 10))
-        screen.blit(drawing_surface, (0,0))
+        screen.blit(image, (0, 0))
+        screen.blit(drawing_surface, (0, 0))
 
         pygame.display.flip()
         clock.tick(60)  # Ограничить до 60 кадров в секунду
