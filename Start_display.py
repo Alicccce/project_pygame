2ea7a309d47d22690c8cda54b2afddf9e1273dc5
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-01-20 19:55:07 +0300

    main_game

--- Start_display.py
+++ Start_display.py
@@ -6,6 +6,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 PINK = (255, 100, 100)
+HEIGHT, WIDTH = 750, 1300
 
 
 def load_image(name, colorkey=None):
@@ -13,33 +14,59 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+class Button(pygame.sprite.Sprite):
+    image = load_image("start.png")
+
+    def __init__(self, *group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно!!!
+        super().__init__(*group)
+        self.image = Button.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 480
+        self.rect.y = 350
+        self.mask_bus = pygame.mask.from_surface(self.image)
+
+    def update(self, keys):
+        pass
 
 def load(surf):
     pygame.init()
     pygame.font.init()
     running = True
-    size = (1300, 750)
+    all_sprites = pygame.sprite.Group()
     clock = pygame.time.Clock()
     FONT = pygame.font.SysFont("Roboto", 80)
     finished = FONT.render("Смурфики", True, "white")
     finished_rect = finished.get_rect(center=(640, 280))
+    Button(all_sprites)
     imgtr = load_image('trop.png')
     imgg = load_image('roof_g.png')
     imgv = load_image('roof_v.png')
+    roof_g_positions = [(490, -35), (80, -35), (225, -35)]
+    roof_v_positions = [(1085, -68), (720, -68), (840, -68)]
     xx, yy = 0, 45
+    for x,y in roof_g_positions:
+        sprite = pygame.sprite.Sprite()
+        sprite.image = imgg
+        sprite.rect = sprite.image.get_rect()
+        all_sprites.add(sprite)
+        sprite.rect.x = x
+        sprite.rect.y = y
+    for x,y in roof_v_positions:
+        sprite = pygame.sprite.Sprite()
+        sprite.image = imgv
+        sprite.rect = sprite.image.get_rect()
+        all_sprites.add(sprite)
+        sprite.rect.x = x
+        sprite.rect.y = y
 
 
 
     while running:
         surf.fill((150, 190, 100))
-        surf.blit(finished, finished_rect)
         screen.blit(imgtr, (xx, yy))
-        surf.blit(imgg,(490, -35))
-        surf.blit(imgg, (80, -35))
-        surf.blit(imgg, (225, -35))
-        surf.blit(imgv, (1085, -68))
-        surf.blit(imgv, (720, -68))
-        surf.blit(imgv, (840, -68))
+
+
 
 
 
@@ -48,6 +75,9 @@ def load(surf):
             if event.type == pygame.QUIT:
                 running = False
 
+        keys = pygame.key.get_pressed()
+        all_sprites.draw(surf)
+        all_sprites.update(keys)
         pygame.display.flip()
         clock.tick(30)
 
