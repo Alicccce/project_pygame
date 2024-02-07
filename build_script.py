40d4588b9068c433e3e70364614ceb44498201b4
Vladislav-Lisin <151949674+Vladislav-Lisin@users.noreply.github.com>
2024-02-07 15:04:49 +0300

    Delete build_script.py

--- build_script.py
+++
@@ -1,8 +0,0 @@
-import PyInstaller.__main__
-
-PyInstaller.__main__.run([
-    'pygame_script.py',
-    '--onefile',        # Создать один исполняемый файл
-    '--name=myexecutable',  # Имя выходного файла (без расширения)
-    '--clean',          # Очистить временные файлы после сборки
-])
