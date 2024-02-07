baf0f30cd2678e5ee63640a1ac1cf36de7b12f3d
Lisin-Vladislav <M1LkMenIO@yandex.ru>
2024-02-07 14:51:37 +0300

    amin

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
-from source.Start_display import start
+# from source.Start_display import start-
 # from source.Alice import game_one
-# from source.game_two import game_two
+from source.game_two import game_two
 # from source.load_img import load
 
 def play():
     screen = pygame.display.set_mode((1300, 750))
-    start(screen)
+    game_two(screen)
 
 play()
